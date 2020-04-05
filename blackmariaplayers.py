92745cbbf3f9c3ee9e7d6e0972e5976f1446e40a
Jake Ireland <os19582@bristol.ac.uk>
2020-04-05 14:06:42 +0100

    fixed remove card problem with random player

--- blackmariaplayers.py
+++ blackmariaplayers.py
@@ -1,30 +1,10 @@
 from random import randint
 
-class RandomPlayer(object):
+class Player(object)
     def __init__(self, table_pos):
         self.table_pos = table_pos
         self.hand = []
         self.tricks = []
-
-    def giveCard(self, suit, preceeding_cards):
-        if suit == "":
-            n = randint(0,len(self.hand)-1)
-            chosen_card = self.hand[n]
-
-        cards_in_suit = []
-        for card in self.hand:
-            if card.suit == suit:
-                cards_in_suit.append(card)
-        if len(cards_in_suit) == 0:
-            n = randint(0,len(self.hand)-1)
-            chosen_card = self.hand[n]
-            self.hand.remove(chosen_card)
-            return chosen_card
-        else:
-            n = randint(0,len(cards_in_suit)-1)
-            chosen_card = cards_in_suit[n]
-            self.hand.remove(chosen_card)
-            return chosen_card
 
     def get_score(self):
         score = 0
@@ -38,3 +18,47 @@ class RandomPlayer(object):
         for card in self.hand:
             to_return += card.display_card()
         return to_return
+
+class RandomPlayer(Player):
+    def __init__(self, table_pos):
+        Player.__init__(self, table_pos)
+
+    def giveCard(self, suit, preceeding_cards):
+        if suit == "":
+            n = randint(0,len(self.hand)-1)
+            chosen_card = self.hand[n]
+        else:
+            cards_in_suit = []
+            for card in self.hand:
+                if card.suit == suit:
+                    cards_in_suit.append(card)
+            if len(cards_in_suit) == 0:
+                n = randint(0,len(self.hand)-1)
+                chosen_card = self.hand[n]
+            else:
+                n = randint(0,len(cards_in_suit)-1)
+                chosen_card = cards_in_suit[n]
+        self.hand.remove(chosen_card)
+        return chosen_card
+
+class DuckAndDump(Player):
+    def __init__(self, table_pos):
+        Player.__init__(self, table_pos)
+
+    def giveCard(self, suit, preceeding_cards):
+        if suit == "":
+            n = randint(0,len(self.hand)-1)
+            chosen_card = self.hand[n]
+        else:
+            cards_in_suit = []
+            for card in self.hand:
+                if card.suit == suit:
+                    cards_in_suit.append(card)
+            if len(cards_in_suit) == 0:
+                n = randint(0,len(self.hand)-1)
+                chosen_card = self.hand[n]
+            else:
+                n = randint(0,len(cards_in_suit)-1)
+                chosen_card = cards_in_suit[n]
+        self.hand.remove(chosen_card)
+        return chosen_card
